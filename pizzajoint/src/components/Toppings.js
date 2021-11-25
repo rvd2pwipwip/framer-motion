6dbb4b71cae58c545e702dc9b2220ef0fb37d986
Herve Desrosiers <hdesrosiers@stingray.com>
2021-11-25 09:52:09 -0500

    06-hover animations

--- pizzajoint/src/components/Toppings.js
+++ pizzajoint/src/components/Toppings.js
@@ -1,32 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Toppings = ({ addTopping, pizza }) => {
-  let toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes'];
+  let toppings = [
+    "mushrooms",
+    "peppers",
+    "onions",
+    "olives",
+    "extra cheese",
+    "tomatoes",
+  ];
 
   return (
     <div className="toppings container">
-      
       <h3>Step 2: Choose Toppings</h3>
       <ul>
-        {toppings.map(topping => {
-          let spanClass = pizza.toppings.includes(topping) ? 'active' : '';
+        {toppings.map((topping) => {
+          let spanClass = pizza.toppings.includes(topping) ? "active" : "";
           return (
-            <li key={topping} onClick={() => addTopping(topping)}>
-              <span className={spanClass}>{ topping }</span>
-            </li>
-          )
+            <motion.li
+              key={topping}
+              onClick={() => addTopping(topping)}
+              whileHover={{ scale: 1.1, originX: 0, color: "yellow" }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
+              <span className={spanClass}>{topping}</span>
+            </motion.li>
+          );
         })}
       </ul>
 
       <Link to="/order">
-        <button>
+        <motion.button
+          whileHover={{
+            scale: 1.1,
+            textShadow: "0px 0px 8px rgb(255,255,255)",
+            boxShadow: "0px 0px 8px rgb(255,255,255)",
+          }}
+          transition={{ type: "spring", stiffness: 120 }}
+        >
           Order
-        </button>
+        </motion.button>
       </Link>
-
     </div>
-  )
-}
+  );
+};
 
 export default Toppings;
