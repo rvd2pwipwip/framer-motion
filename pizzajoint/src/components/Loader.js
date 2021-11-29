f1d430543ce023a2db163c156b3a28d24888bb5f
Herve Desrosiers <hdesrosiers@stingray.com>
2021-11-29 06:26:44 -0500

    16-loaders

---
+++ pizzajoint/src/components/Loader.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const loaderVariants = {
+  start: {
+    x: [-20, 20],
+    y: [0, -30],
+    transition: {
+      x: {
+        yoyo: Infinity,
+        duration: 0.5,
+      },
+      y: {
+        yoyo: Infinity,
+        duration: 0.25,
+        ease: "easeOut"
+      },
+    },
+  },
+  end: {},
+};
+
+const Loader = () => {
+  return (
+    <>
+      <motion.div
+        className="loader"
+        variants={loaderVariants}
+        animate="start"
+      ></motion.div>
+    </>
+  );
+};
+
+export default Loader;
